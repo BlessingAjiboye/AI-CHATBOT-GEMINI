4ba3a2fa8177f12e5572994c97f964e1c23f3f2e
Blessing Ajiboye <blessjiboye@yahoo.com>
2024-12-16 22:19:20 -0500

    first commit

---
+++ .history/chatbot/vite.config_20241024142120.js
@@ -0,0 +1,33 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+*/
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const { PORT = 5000} = process.env;
+
+export default defineConfig({
+ plugins: [react()],
+ server:{
+     proxy:{
+     //'/api':{
+     target:`http://localhost:${PORT}`,
+     changeOrigin: true,
+     //},
+     
+     },
+    },
+    
+ build: {
+ manifest: true,
+ rollupOptions: {
+ input: "./src/main.jsx",
+ },
+ },
+});
